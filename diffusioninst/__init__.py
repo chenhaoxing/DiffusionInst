67b2c0462946d20c427cf1ce896329e281a9df1a
Zhangxuan Gu <zhangxgu@126.com>
2022-12-09 23:14:58 +0800

    Update __init__.py

---
+++ diffusioninst/__init__.py
@@ -0,0 +1,5 @@
+from .config import add_diffusioninst_config
+from .detector import DiffusionInst
+from .dataset_mapper import DiffusionInstDatasetMapper
+from .test_time_augmentation import DiffusionInstWithTTA
+from .swintransformer import build_swintransformer_fpn_backbone
